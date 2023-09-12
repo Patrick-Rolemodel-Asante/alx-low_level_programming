2d8ee641166a7f2daebf12646a54d55eaf139a91
PATRICK ROLEMODEL ASANTE <alxse19.1@gmail.com>
2023-09-12 06:15:54 -0700

    make changes\

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to check
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
-	int a;
+        int a;
 
-	if (n < 0)
-	n = -n;
+        if (n < 0)
+        n = -n;
 
-	a = n % 10;
+        a = n % 10;
 
-	if (a < 0)
-		a = -a;
+        if (a < 0)
+                a = -a;
 
-	_putchar(a + '0');
+        _putchar(a + '0');
 
-	return (a);
+        return (a);
 }
 
