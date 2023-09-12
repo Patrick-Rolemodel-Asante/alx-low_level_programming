bdbb876df141e2277e66e18152134310d41e6f21
PATRICK ROLEMODEL ASANTE <alxse19.1@gmail.com>
2023-09-12 06:32:16 -0700

    make changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,20 @@
  */
 int print_sign(int n)
 {
-	if (n>0)
-	{
-	_putchar(43);
-	return (1);
-	}
-	else if (n<0)
-	{
-	_putchar(45);
-	return (-1);
-	}
-	else 
-	{
-	_putchar(48);
-	return (0);
-	}
+        if (n > 0) // spaces required around '>'
+        {
+        _putchar(43);
+        return (1);
+        }
+        else if (n < 0) // spaces required around '<'
+        {
+        _putchar(45);
+        return (-1);
+        }
+        else 
+        {
+        _putchar(48); // remove trailing whitespace
+        return (0);
+        }
 }
+
