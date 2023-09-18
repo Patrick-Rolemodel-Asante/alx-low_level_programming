75bf61768b137f1069dbaee9191491f31589f6a8
PATRICK ROLEMODEL ASANTE <alxse19.1@gmail.com>
2023-09-18 09:01:26 -0700

    make corrections

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a given string
- * @s: the string to be reversed
- *
- * Return: string in reversed form
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
 
 void rev_string(char *s)
-{       
-        char rev = s[0];
-        int count = 0;
-        int i;
-        
-        while (s[count] != '\0')
-                count++;
-        
-        for (i=0; i<count; i++)
-        {       
-                count --;
-                rev = s[i];
-                s[i] = s[count];
-                s[count] = rev;
-        } 
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
+
