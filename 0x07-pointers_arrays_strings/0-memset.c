e1af77db8d03687c8eac644443a32e5ca7448244
PATRICK ROLEMODEL ASANTE <alxse19.1@gmail.com>
2023-09-25 06:16:21 -0700

    make changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,6 +15,6 @@ char *_memset(char *s, char b, unsigned int n)
 	int i = 0;
 
 	for (; i < n; i++)
-		*(s + i) = b;
+		s[i] = b;
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,7 +15,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int i = 0;
 
 	for (; i <= n; i++)
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 
 	return (dest);
 }
