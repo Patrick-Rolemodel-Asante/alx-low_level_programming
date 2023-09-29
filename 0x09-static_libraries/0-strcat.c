0e3cd3f5220f80980bee6bdee33e823c344d4cf7
PATRICK ROLEMODEL ASANTE <alxse19.1@gmail.com>
2023-09-29 09:15:23 -0700

    add file

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,11 +1,13 @@
 #include "main.h"
- /**
- *_strcat - concatenates two strings
- * @dest: input value
- * @src: input value
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: the destination string
+ * @src: the source string
  *
- * Return: void
+ * Return: a pointer to the resulting string dest
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
