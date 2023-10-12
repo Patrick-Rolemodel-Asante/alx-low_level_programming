334a99ec4fdf28dc703987362233c13c8624c528
Rol <alxse19.1@gmail.com>
2023-10-12 20:38:16 +0000

    modified 0-sum_them_all.c once more

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
@@ -23,3 +24,4 @@ int sum_them_all(const unsigned int n, ...)
 
 	return (sum);
 }
+
