7a44ce96e2b58d184103f2df2d2911809e0eef03
Rol <alxse19.1@gmail.com>
2023-10-20 19:15:56 +0000

    make changes

--- 0x12-singly_linked_lists/101-hello_holberton.s
+++ 0x12-singly_linked_lists/101-hello_holberton.s
@@ -1,9 +1,19 @@
-          global    main
-          extern    printf
+section .data
+    hello db 'Hello, Holberton', 0
+
+section .text
+    extern printf
+    global main
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+    ; Prepare arguments for printf
+    mov rdi, hello     ; format
+    xor rax, rax       ; clear rax to set up for variadic function
+
+    ; Call printf
+    call printf
+
+    ; Return from main
+    mov eax, 0x60      ; syscall number (sys_exit)
+    xor edi, edi       ; exit code
+    syscall            ; call kernel
