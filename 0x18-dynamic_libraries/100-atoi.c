1c9fe5fcabc36e5e39c3b207fce4a9221d4fe1ac
Rol <alxse19.1@gmail.com>
2023-12-05 14:26:43 +0000

    add ./len

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _atoi - Convert a string to an integer
+ * @s: Pointer to the string to convert
+ *
+ * Return: The integer that was converted
+ */
+
+int _atoi(char *s)
+
+{
+	return (atoi(s));
+}
+
